e3c3ef5aac0d25c26af233e928a671d164413a56
Cory Dolphin <wcdolphin@gmail.com>
2013-01-27 22:14:51 -0500

    Update documentation and embed version in python

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
-
+from flask_olinauth import __version__
+import os
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='Flask-OlinAuth',
-    version='1.2.0',
+    version=__version__,
     url='https://github.com/wcdolphin/flask-olinauth',
     license='MIT',
     author='Cory Dolphin',
     author_email='wcdolphin@gmail.com',
     description='A simple Flask extension implementing Olin\'s authentication',
-    long_description=__doc__,
+    long_description= open(os.path.join(here, "README.md")).read(),
     py_modules=['flask_olinauth'],
     # if you would be using a package instead use packages instead
     # of py_modules:
